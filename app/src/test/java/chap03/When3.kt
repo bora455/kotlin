68aec0c581f77c98128455a0b1c5f8e5fd89b8b7
kimbomi <bomi258@naver.com>
2023-02-15 10:25:17 +0900

    인자를 사용하는 when문

---
+++ app/src/test/java/chap03/When3.kt
@@ -0,0 +1,17 @@
+package chap03
+
+fun main() {
+    var x = 5
+    when (x) {
+        in 1..10 -> print("x는 1이상 10 이하입니다.")
+        !in 10..20 -> print("x는 10 이상 20 이하의 범위에 포함되지 않습니다.")
+        else -> print("x는 어떤 범위에도 없습니다.")
+    }
+    print ("\n")
+
+    val str = "안녕하세요."
+    val result = when(str) {
+        is String -> print("문자열입니다.")
+        else -> print("false")
+    }
+}
