53605aa9429168fa07710651cad42cd5514029fc
kimbomi <bomi258@naver.com>
2023-02-23 10:42:18 +0900

    메서드에 지역 클래스 추가해보기

--- app/src/test/java/chap08/InnerClassTest2.kt
+++ app/src/test/java/chap08/InnerClassTest2.kt
@@ -8,10 +8,11 @@ class Smartphone2(val model: String) {
         //바깥 클래스의 프로퍼티 접근
     }
     fun powerOn(): String {
-        class Led(val color: String) {
+        class Led(val color: String) { //지역 클래스 선언
             fun blink(): String = "Blinking $color on $model"
+            //외부의 프로퍼티는 접근 가능
         }
-        val powerStatus = Led("Red")
+        val powerStatus = Led("Red") //여기에서 지역 클래스가 사용됨
         return powerStatus.blink()
     }
 }
