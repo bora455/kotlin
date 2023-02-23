bce5f265a1924fcd9d5fbeb1090d67bb928c453d
kimbomi <bomi258@naver.com>
2023-02-23 10:20:26 +0900

    이너 클래스의 바깥 클래스의 멤버 접근

---
+++ app/src/test/java/chap08/InnerClassTest.kt
@@ -0,0 +1,14 @@
+package chap08
+
+class Smartphone(val model: String) {
+    private val cpu = "Exynos"
+
+    inner class ExternalStorage(val size: Int) {
+        fun getInfo() = "${model}: Installed on $cpu with ${size}GB"
+        //바깥 클래스의 프로퍼티 접근
+    }
+}
+fun main() {
+    val mySdcard = Smartphone("s7").ExternalStorage(32)
+    println(mySdcard.getInfo())
+}
