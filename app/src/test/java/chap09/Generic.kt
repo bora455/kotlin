b38b88fe8a579769418fad394f993b0b4a896af1
kimbomi <bomi258@naver.com>
2023-02-24 11:10:00 +0900

    람다식

---
+++ app/src/test/java/chap09/Generic.kt
@@ -0,0 +1,10 @@
+package chap09
+
+fun <T> add(a: T, b: T, op: (T, T)->T): T {
+    return op(a, b)
+}
+fun main() {
+    val result = add(2, 3, {a, b->a+b})
+    //val result = add(2, 3) {a, b->a+b}
+    println(result)
+}
