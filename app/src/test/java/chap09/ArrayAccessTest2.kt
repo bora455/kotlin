4dfe21af9f90b8e628042adddb8e4feb03d9bbc3
kimbomi <bomi258@naver.com>
2023-02-24 12:00:25 +0900

    배열 선언 및 접근 연습

---
+++ app/src/test/java/chap09/ArrayAccessTest2.kt
@@ -0,0 +1,24 @@
+package chap09
+
+import java.util.Arrays //Arrays를 사용하기 위해
+
+fun main(args: Array<String>) {
+    val arr = intArrayOf(1, 2, 3, 4, 5, 8)
+    println("arr: ${Arrays.toString(arr)}") //배열의 내용을 문자열로 변환함
+    println("size: ${arr.size}") //배열의 크기
+    println("sum(): ${arr.sum()}") //배열의 합
+
+    //게터에 의한 접근과 대괄호 연산자 표기법
+    println(arr.get(2))
+    println(arr[2])
+
+    //세터에 의한 값의 설정
+    arr.set(1, 5)
+    arr[0] = 3
+    println("size: ${arr.size}, arr[0]: ${arr[0]}, arr[2]: ${arr[2]}")
+
+    //루프를 통한 배열 요소의 접근
+    for (i in 0..arr.size -1) {
+        println("arr[$i] = ${arr[i]}")
+    }
+}
