095a3cb6eb33821a8233def74fd8033c6ee4cd4f
kimbomi <bomi258@naver.com>
2023-02-24 11:13:33 +0900

    무변성

---
+++ app/src/test/java/chap09/Invariance.kt
@@ -0,0 +1,8 @@
+package chap09
+
+class Box<T>(val size: Int)
+
+fun main(args: Array<String>) {
+    //val anys: Box<Any> = Box<Int>(10) //자료형 불일치로 오류
+    //val nothings: Box<Nothing> = Box<Int>(20) //자료형 불일치로 오류
+}
